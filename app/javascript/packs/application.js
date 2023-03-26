a3004cce29e9ac2d14144c1cafb013fdcc95e43c
naon708 <naoya_nis@icloud.com>
2023-03-26 18:47:46 +0900

    Turbolinksが発火していること確認した

--- app/javascript/packs/application.js
+++ app/javascript/packs/application.js
@@ -8,6 +8,10 @@ require("turbolinks").start()
 require("@rails/activestorage").start()
 require("channels")
 
+// turbolinksが発火するたびにconsoleにメッセージを表示する
+document.addEventListener("turbolinks:load", function() {
+  console.log("turbolinks:load fire")
+})
 
 // Uncomment to copy all static images under ../images to the output folder and reference
 // them with the image_pack_tag helper in views (e.g <%= image_pack_tag 'rails.png' %>)
